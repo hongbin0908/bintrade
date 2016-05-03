93b747002d31746c7aed8118040561ee4f971a35
U-DESKTOP-PP9PIE6\abin <you@example.com>
2016-05-03 21:42:52 +0800

    bug fix

--- ml/diff_feature_cls.py
+++ ml/diff_feature_cls.py
@@ -4,27 +4,15 @@ import json
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier,GBTClassifier,LogisticRegression
 from pyspark.ml.feature import StringIndexer, VectorIndexer
-from pyspark.mllib.classification import LogisticRegressionWithLBFGS
-from pyspark.mllib.linalg import Vectors, Vector, DenseVector
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.tree import GradientBoostedTrees
-from pyspark.mllib.classification import LogisticRegressionWithLBFGS
 from pyspark.mllib.linalg import Vectors
-from pyspark.mllib.regression import LabeledPoint
-from pyspark import SparkConf, SparkContext, HiveContext
-from pyspark.sql.types import StructType,StructField,StringType,IntegerType,FloatType
 from bintrade_tests.test_lib import *
 
 local_path = os.path.dirname(__file__)
 
-
 def cal_feature_per(x, window, coach, threshold):
     assert window >= 2
-    if len(x) == 0:
-        return []
-    l = []
+    assert len(x) > 0
     x.sort(lambda xx,yy: cmp(xx.date, yy.date),reverse=False)
-
 
     for i in range(0, len(x) - window):
         l_idx = []
@@ -145,94 +133,6 @@ def cal_index_per(x):
         l.append(dx)
     return l
 def get_lp(sc, sql_context, is_hive):
-    #df_sym = sql_context.sql("""
-    #SELECT
-    #    1 AS du,
-    #    symbol
-    #FROM
-    #    eod_rel
-    #GROUP BY
-    #    symbol
-    #ORDER BY
-    #    symbol asc
-    #""")
-
-    #df_sym = df_sym.rdd.groupBy(lambda x: x.du)\
-    #      .map(lambda x: (x[0], list(x[1])))\
-    #      .flatMapValues(lambda x: cal_index_per(x))\
-    #      .map(lambda x: x[1])
-    #df_sym = sql_context.createDataFrame(df_sym)
-    #print df_sym.first()
-    #df_sym = df_sym.withColumn("id_num", df_sym.du*df_sym.count())
-
-    #df_close = sql_context.sql("""
-    #SELECT
-    #    symbol,
-    #    date,
-    #    open  as open,
-    #    close as close
-    #FROM
-    #    eod_rel
-    #""")
-
-    #df_close = df_close.join(df_sym, [df_close.symbol == df_sym.symbol], 'inner')\
-    #                   .select(df_close.symbol.alias("symbol"),
-    #                           df_sym.id.alias("id"),
-    #                           df_sym.id_num.alias("id_num"),
-    #                           df_close.date.alias("date"),
-    #                           df_close.open.alias("open"),
-    #                           df_close.close.alias("close"))
-
-    #print "df_close:", df_close.count()
-    #print df_close.orderBy(df_close.date.desc()).first()
-
-    #df_mat = sql_context.sql("""
-    #SELECT
-    #    mat1.symbol AS symbol,
-    #    mat2.date AS date,
-    #    mat1.close_mat*1.0/mat2.close_mat AS mat_dual
-    #FROM
-    #    ta_mat20_close mat1
-    #INNER JOIN
-    #    ta_mat8_close mat2
-    #ON
-    #    mat1.symbol = mat2.symbol
-    #    AND mat1.date = mat2.date
-    #WHERE
-    #    mat1.close_mat > 0
-    #""")
-    #print "df_mat:", df_mat.count()
-    #print df_mat.orderBy(df_mat.date.desc()).first()
-
-    #df_adx = sql_context.sql("""
-    #SELECT
-    #    symbol,
-    #    date,
-    #    pdi14,
-    #    mdi14,
-    #    adx
-    #FROM
-    #    ta_adx
-    #WHERE
-    #    adx > 0
-    #""")
-
-    #df_lp = df_close.join(df_mat, [df_close.symbol == df_mat.symbol, df_close.date == df_mat.date], 'inner' )\
-    #                .join(df_adx, [df_close.symbol == df_adx.symbol, df_close.date == df_adx.date], 'inner' )\
-    #                .select(df_close.symbol.alias("symbol"),
-    #                        df_close.id.alias("id"),
-    #                        df_close.id_num.alias("id_num"),
-    #                        df_close.date.alias("date"),
-    #                        df_close.open.alias("open"),
-    #                        df_close.close.alias("close"),
-    #                        df_mat.mat_dual.alias("mat_dual"),
-    #                        df_adx.pdi14.alias("pdi14"),
-    #                        df_adx.mdi14.alias("mdi14"),
-    #                        df_adx.adx.alias("adx"))
-    #print "df_lp:", df_lp.count()
-
-    #for each in df_lp.orderBy(df_lp.date.desc()).take(10):
-    #    print each
     df_lp = sql_context.read.table("ta_merge")
     return df_lp
 
@@ -267,6 +167,7 @@ def cal_prob(x,label2index):
     dx["prob"] = float(dx["probability"].toArray()[label2index[1]])
     del dx["probability"] #= str(dx["probability"].toArray().tolist())
     del dx["rawPrediction"]
+    return dx
 def cal_prob2(x,label2index):
     dx = {}
     dx["symbol"] = x.symbol
@@ -321,7 +222,7 @@ def run(start1, end1, start2, end2, df, sc, sql_context, fout):
 
     lp_cur = lp_data.filter(lp_data.is_labeled==0)
     predictions = model.transform(lp_cur)
-    predictions = sql_context.createDataFrame(predictions.rdd.map(lambda x: cal_prob(x, label2index)))
+    predictions = sql_context.createDataFrame(predictions.rdd.map(lambda x: cal_prob2(x, label2index)))
     predictions = predictions.sort(predictions.prob.desc())
     dfToCsv(predictions, fout)
 def main(window, coach, sc, sql_context, is_hive = True):
